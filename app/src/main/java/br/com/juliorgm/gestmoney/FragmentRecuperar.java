a4432c25332d781d4868aebe8d96dac6bd7f137a
Danilo <danilo_santiago96@hotmail.com>
2019-04-09 21:00:01 -0300

    Gest

--- app/src/main/java/br/com/juliorgm/gestmoney/FragmentRecuperar.java
+++ app/src/main/java/br/com/juliorgm/gestmoney/FragmentRecuperar.java
@@ -12,37 +12,23 @@ import android.view.ViewGroup;
 
 public class FragmentRecuperar extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public FragmentRecuperar() {
 
     }
 
 
-    public static FragmentRecuperar newInstance(String param1, String param2) {
-        FragmentRecuperar fragment = new FragmentRecuperar();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,29 +37,7 @@ public class FragmentRecuperar extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment_recuperar, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
 
     public interface OnFragmentInteractionListener {
--- app/src/main/java/br/com/juliorgm/gestmoney/MainActivity.java
+++ app/src/main/java/br/com/juliorgm/gestmoney/MainActivity.java
@@ -1,29 +1,43 @@
 package br.com.juliorgm.gestmoney;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentLogin.OnFragmentInterectionListener,FragmentRegistrar.OnFragmentInterectionListener,FragmentRecuperar.OnFragmentInteractionListener {
+
+    private FrameLayout cont_fragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.cont_fragment, new FragmentLogin()).commit();
+        }
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 
 }
+
+
